96c56f0990b1ea006e3fe5157c9d1772bb6de8c9
Mukul Pal <mukulpal0333@gmail.com>
2026-05-07 09:44:36 +0530

    feat: setup prisma with singleton pattern and pg adapter

---
+++ backend/src/libs/db.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
+
+const prismaClientSingleton = () => {
+  const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+  const adapter = new PrismaPg(pool);
+  return new PrismaClient({ adapter });
+};
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prismaGlobal: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prismaGlobal ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prismaGlobal = prisma;
